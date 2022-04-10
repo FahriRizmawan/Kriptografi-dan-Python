06051fc249b14dc1b9356ce724515f35ab962267
Fahri Rizmawan <85377118+FahriRizmawan@users.noreply.github.com>
2022-04-10 14:00:43 +0700

    commit Vigenere Chiper

---
+++ vigenere_chiper.py
@@ -0,0 +1,73 @@
+import pyperclip
+
+ALHPABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+
+# di dalam fungsi main terdapat variabel pesan, kunci dan mode sebelum program dijalankan
+# pada modeSaya anda bisa menetapkan apakah program akan melakukan enkripsi atau dekripsi
+def main():
+    pesanSaya = """Nim: 19102138, nama: Fahri Rizmawan, isi pesan: ini adalah tugas dua kriptografi kelas TI dua"""
+    kunciSaya = 'Informatika19'
+    #untuk melakukan proses enkripsi atau dekripsi
+    modeSaya = 'enkripsi'
+
+    if modeSaya == 'enkripsi':
+        ubah = enkripsiPesan(kunciSaya, pesanSaya)
+    elif modeSaya == 'dekripsi':
+        ubah = dekripsiPesan(kunciSaya, pesanSaya)
+
+    print('%sed pesan : ' % (modeSaya.title()))
+    print(ubah)
+    pyperclip.copy(ubah)
+    print()
+    print('Pesan akan disalin kedalam papan klip.')
+
+
+# pada bagian ini menjelaskan bagaimana enkripsi berjalan
+def enkripsiPesan(kunci, pesan):
+    return ubahPesan(kunci, pesan, 'enkripsi')
+
+
+# pada bagian ini menjelaskan bagaimana dekripsi berjalan
+def dekripsiPesan(kunci, pesan):
+    return ubahPesan(kunci, pesan, 'dekripsi')
+
+
+def ubahPesan(kunci, pesan, mode):
+    ubah = []
+    # menyimpan pesan enkripsi dan dekripsi
+
+    kunciIndex = 0
+    kunci = kunci.upper()
+
+    for symbol in pesan:
+        # akan dilakukan pada seluruh karakter dalam pesan
+        nomor = ALHPABET.find(symbol.upper())
+        if nomor != -1:  # -1 berarti symbol.upper() tidak ditemukan didalam HURUF
+            if mode == 'enkripsi':
+                nomor += ALHPABET.find(kunci[kunciIndex])  # tambahkan jika dienkripsi
+            elif mode == 'dekripsi':
+                nomor -= ALHPABET.find(kunci[kunciIndex])  # kurangi jika melakukan dekripsi
+
+            nomor %= len(ALHPABET)
+
+            # tambahkan pada hasil symbol enkrip/dekrip yang sudah diubahkan
+            if symbol.isupper():
+                ubah.append(ALHPABET[nomor])
+            elif symbol.islower():
+                ubah.append(ALHPABET[nomor].lower())
+
+            kunciIndex += 1
+            # ubah kunci yang akan dipakai selanjutnya
+            if kunciIndex == len(kunci):
+                kunciIndex = 0
+
+        else:
+            # symbol tidak berada pada Alphabet, maka tambahkan hal tersebut dan ubahkan
+            ubah.append(symbol)
+
+    return ''.join(ubah)
+
+# panggil fungsi main untuk menjalankan programnya
+if __name__ == '__main__':
+    main()
